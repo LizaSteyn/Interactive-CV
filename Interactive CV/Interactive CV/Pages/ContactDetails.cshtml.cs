49f6001c9e1c8bd30e7818375663b6433c848eff
LizaSteyn <71076486+LizaSteyn@users.noreply.github.com>
2021-11-30 18:12:52 +0200

    About

---
+++ Interactive CV/Interactive CV/Pages/ContactDetails.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Interactive_CV.Pages
+{
+    public class ContactDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
